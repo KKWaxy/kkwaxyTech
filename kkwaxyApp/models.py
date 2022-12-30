b956d90f8a7abfab251fd00e9a457bc44cdb7f3f
Wilfried Kouassi <wilfried.kouassi@veone.net>
2022-12-30 14:36:33 +0000

    Comment cv

--- kkwaxyApp/models.py
+++ kkwaxyApp/models.py
@@ -2,11 +2,24 @@ from django.contrib.auth.models import User
 from django.db import models
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.models import User
 
+def user_directory_path(instance: models.Model, filename: str)->str:
+    
+    return 'users/{0}/{1}'.format(instance.user.id, filename)
+
+class Profile(models.Model):
+
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    avatar = models.ImageField(_("Avatar"),upload_to=user_directory_path, default="")
+    bio = models.TextField(_("Biographie"), max_length=500, null=True, blank=True)
+   # cv = models.FieldFile()
+    
 class Tag(models.Model):
     name = models.CharField(_("Nom"),max_length=25,null=False,blank=False)
     updated_date = models.DateTimeField(_("Created"),auto_now=True)
     created_date = models.DateTimeField(_("Updated"),auto_now_add=True)
+    
     class Meta:
         verbose_name = _("Tag")
         verbose_name_plural = _("Tags")
